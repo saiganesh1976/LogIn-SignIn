cd25c2165f1742a2267a7ef72c112f1dd05919b9
Sai Ganesh Ratnala <125180139+saiganesh1976@users.noreply.github.com>
2023-07-04 22:22:19 +0530

    Update SIGNUP.js

--- SIGNUP.js
+++ SIGNUP.js
@@ -1,15 +1,14 @@
 function validateForm() {
     let x = document.getElementById("email").value;
-    let y = document.getElementById("pwd").value;
-    let z = document.getElementById("FNAME").value;
-    let p = document.getElementById("LNAME").value;
+    let y = document.getElementById("fname").value;
+    let z = document.getElementById("lname").value;
     let q = document.getElementById("pwd").value;
     let r = document.getElementById("PNO").value;
-    if (x  == "" && y=="" && z=="" && p==" " && q=="" && r=="" ) {
+    if (x  == "" && y=="" && z=="" && q=="" && r=="" ) {
       alert("Fill all the Entries");
       return false;
     }
     else{
-      alert("Successfully Created");
+      alert(" Account created Successfully");
     }
   }
